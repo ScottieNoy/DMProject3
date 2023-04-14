3f823dc5e0a5d61493409d8ff8d696823be2e0b1
ScottieNoy <91637465+ScottieNoy@users.noreply.github.com>
2023-04-14 09:06:42 +0200

    with initial classes

---
+++ src/generateWebServer.h
@@ -0,0 +1,4 @@
+//This is the header file for the function generateWebServer() in src/generateWebServer.cpp
+//This is the declaration of the function generateWebServer() in src/generateWebServer.cpp
+
+void generateWebServer();
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#include "connectWiFi.h"
+#include "generateWebServer.h"
+#include <WebServer.h>
+
+/// --------- WiFi credentials -------- ///
+
+char* ssid = "Sophus - iPhone";
+char* password = "sophus123";
+
+/// ---------------------------------- ///
+
+/// --------- WebServer Port -------- ///
+
+WebServer server(80);
+
+/// --------------------------------- ///
+
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  connect(ssid, password);
+  generateWebServer();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  server.handleClient();
 }
